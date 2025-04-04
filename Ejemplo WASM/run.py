9f3cc1850a52eb08cf9314f3e697df6970f177cd
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-04-03 19:13:56 -0600

    Add Fahrenheit to Celsius conversion function and update run.py with additional test cases

--- Ejemplo WASM/run.py
+++ Ejemplo WASM/run.py
@@ -14,6 +14,10 @@ def call_wasm_fun(file_name, fn_name, *args):
 def main():
     print(call_wasm_fun('simple.wat', 'meaning_of_life'))
     print(call_wasm_fun('simple.wat', 'multiply', 2, 3))
+    print(call_wasm_fun('simple.wat', 'multiply', 7, 11))
+    print(call_wasm_fun('simple.wat', 'f2c', 212.0))  # 100.0
+    print(call_wasm_fun('simple.wat', 'f2c', 32.0))   # 0.0
+    print(call_wasm_fun('simple.wat', 'f2c', -40.0))  # -40.0
 
 
 if __name__ == '__main__':
--- Ejemplo WASM/simple.wat
+++ Ejemplo WASM/simple.wat
@@ -19,4 +19,21 @@
     local.get $b
     i32.mul
   )
+
+  ;; Convierte de Fahrenheit a Celsius
+  (func
+    (export "f2c")
+    (param $f f64)
+    (result f64)
+
+    local.get $f
+    f64.const 32.0
+    f64.sub
+
+    f64.const 5.0
+    f64.mul
+
+    f64.const 9.0
+    f64.div
+  )
 )
